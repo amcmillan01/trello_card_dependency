8531bfcf94e7c2bdb0ce502e6c3a6d34508f4286
Andrei McMillan <amcmillan@yieldbot.com>
2016-04-05 22:43:08 -0400

    update description

--- src/bg/index.js
+++ src/bg/index.js
@@ -5,13 +5,9 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status === 'complete' && tab.url.match(/\/c(ards)?\//i)) {
     // send message to content script
 
-    // var parts = tab.url.replace(/https?:\/\//, '').split('/');
-
     chrome.tabs.sendMessage(tabId, {
       __ns: 'card_dependency',
       url: tab.url,
-      // parts: parts,
-      // shortlink: parts[2],
     });
   }
 });
